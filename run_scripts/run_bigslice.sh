58e56aea39f59daf5dbbab432d4f1e8f50d18c03
pereiramemo <pereiramemo@gmail.com>
2023-08-21 20:53:42 +0000

    updated

--- run_scripts/run_bigslice.sh
+++ run_scripts/run_bigslice.sh
@@ -16,6 +16,10 @@ function realpath() {
   cd "${CURRENT_DIR}"
 }
 
+# handle mode
+readonly MODE="${1}"
+shift
+
 # handle input dir
 readonly INPUT_DIR_PATH=$(dirname $(realpath "${1}"))
 readonly INPUT_DIR_NAME=$(basename $(realpath "${1}"))
@@ -26,17 +30,9 @@ readonly OUTPUT_DIR_PATH=$(dirname $(realpath "${1}"))
 readonly OUTPUT_DIR_NAME=$(basename $(realpath "${1}"))
 shift
 
-# handle reference db
-if [[ -d "${1}" ]]; then
-  REF_DB_DIR_PATH=$(dirname $(realpath "${1}"))
-  REF_DB_DIR_NAME=$(basename $(realpath "${1}"))
-  shift
-fi  
-
 # Links within the container
 readonly CONTAINER_SRC_DIR=/input
 readonly CONTAINER_DST_DIR=/output
-readonly CONTAINER_REF_DIR=/resources
 
 if [[ ! -d "${OUTPUT_DIR_PATH}" ]]; then
     mkdir "${OUTPUT_DIR_PATH}"
@@ -49,14 +45,12 @@ fi
 # echo "input dir name: ${INPUT_DIR_NAME}"
 # echo "output dir path: ${OUTPUT_DIR_PATH}"
 # echo "output dir name: ${OUTPUT_DIR_NAME}"
-# echo "ref db dir path: ${REF_DB_DIR_PATH}"
-# echo "ref db dir name: ${REF_DB_DIR_NAME}"
 
 ###############################################################################
 # 2. Run bigslice in Mode 1: clustering input BGCs
 ###############################################################################
 
-if [[ -z "${REF_DB_DIR}" ]]; then
+if [[ "${MODE}" == "cluster" ]]; then
   docker run \
       --volume ${INPUT_DIR_PATH}:${CONTAINER_SRC_DIR}:ro \
       --volume ${OUTPUT_DIR_PATH}:${CONTAINER_DST_DIR}:rw \
@@ -73,17 +67,15 @@ fi
 # 3. Run bigslice in Mode 2: quering input BGCs against reference DB
 ###############################################################################
 
-if [[ ! -z "${REF_DB_DIR}" ]]; then
+if [[ "${MODE}" == "query" ]]; then
   docker run \
       --volume ${INPUT_DIR_PATH}:${CONTAINER_SRC_DIR}:ro \
       --volume ${OUTPUT_DIR_PATH}:${CONTAINER_DST_DIR}:rw \
-      --volume ${REF_DB_DIR_PATH}:${CONTAINER_REF_DIR}:ro \
       --detach=false \
       --rm \
       --user=$(id -u):$(id -g) \
       epereira/bigslice:latest \
-      -i ${CONTAINER_SRC_DIR}/${INPUT_DIR_NAME} \
-      --query ${CONTAINER_REF_DIR}/${REF_DB_DIR_NAME} \
+      --query ${CONTAINER_SRC_DIR}/${INPUT_DIR_NAME} \
       $@ \
       ${CONTAINER_DST_DIR}/${OUTPUT_DIR_NAME} 
 fi
