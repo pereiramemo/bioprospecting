077b4f7c4a12c9d0a54469a2a70c9e623d02aaf7
pereiramemo <pereiramemo@gmail.com>
2023-11-08 15:32:29 +0000

    removed run_scripts aux_scripts cont_env folders

--- aux_scripts/get_season_south.R
+++
@@ -1,9 +0,0 @@
-library(lubridate)
-get_season_south <- function(input.date){
-  numeric.date <- 100*month(input.date)+day(input.date)
-  cuts <- base::cut(numeric.date, breaks = c(0,320,0621,0922,1221,1231)) 
-  levels(cuts) <- c("Summer","Fall","Winter","Spring","Summer")
-  return(cuts)
-}
-
-
